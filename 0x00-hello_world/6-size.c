957a5dd4515ba7be947d3aad9fcea37ce0f954da
Nguma-01 <ngumaelaine11@gmail.com>
2022-09-09 03:44:35 -0700

    Size of various variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - serves as the entry point of the program
+ *
+ * Return: zero by default and if error return non-zero value
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,3 +4,4 @@ Task 2:The script generates the assembly code of a C code and save it in an outp
 Task 3:The script compiles a $CFILE and creates an executable named cisfun
 Task 4:C program that uses puts function to print exactly "Programming is like building a multilingual puzzle
 Task 5:C program prints "with proper grammar, but the outcome is a piece of art," using printf function
+Task 6:The C program prints the size of various types on the computer it is compiled and run on.
