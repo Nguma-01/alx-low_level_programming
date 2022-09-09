3ddff3eab46d2cf700068498e4ef2f635a4c397e
Nguma-01 <ngumaelaine11@gmail.com>
2022-09-09 04:31:14 -0700

    Woow

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - serves as the entry point of the program
+ *
+ * Return:returns 1 on success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
